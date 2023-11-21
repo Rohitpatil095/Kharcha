e584bd423ddbdbc842ccb361c8aca0bc0e3b8ff7
rpatil <rohitpatil1340sa@gmail.com>
2023-11-21 23:05:47 +0530

    created kharcha app repo, code files inital

---
+++ src/test/java/com/app/kharcha/KharchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kharcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KharchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
